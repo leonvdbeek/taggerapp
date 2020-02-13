61289338708e6ff27cc35c729a4d74c4e72a5eb8
leonvdbeek <leon.vandebeek@xs4all.nl>
2020-02-13 16:41:56 +0700

    try with added div

--- src/components/TagMap.js
+++ src/components/TagMap.js
@@ -11,47 +11,50 @@ import { Button, Typography } from '@material-ui/core';
 export default function TagMap(props) {
 
   return (
-    <LoadingOverlay style={{ height: "100%", position: "fixed" }} active={props.loading} spinner text={<Typography>Loading data..</Typography>}>
-      <GoogleMapProvider>
-        <MapBox
-          apiKey="AIzaSyBemL6idNDqQ9rh3jZvAzq9F6sAfYGBIf4"
-          style={{
-            height: "100vh",
-            width: "100vw",
-            minHeight: "-webkit-fill-available", position: "fixed"
-          }}
-          opts={{
-            center: props.cent,
-            zoom: 6,
-            disableDefaultUI: false,
-            fullscreenControl: false,
-            streetViewControl: false
-          }}
-        />
-        <CustomControl bindingPosition="BOTTOM_CENTER">
-          <Button variant="contained" color="primary" onClick={() => props.setOpenPlace(true)} style={{ marginBottom: "20px" }}>
-            Place tag!
-              </Button>
-        </CustomControl>
-
-        {props.tags.map(tag => (
-          <Marker
-            key={tag._id}
-            id={tag.name}
-            opts={{
-              position: {
-                lat: tag.lat,
-                lng: tag.lng
-              }
+    <div style={{ height: "100%", position: "fixed" }} >
+      <LoadingOverlay active={props.loading} spinner text={<Typography>Loading data..</Typography>}>
+        <GoogleMapProvider>
+          <MapBox
+            apiKey="AIzaSyBemL6idNDqQ9rh3jZvAzq9F6sAfYGBIf4"
+            style={{
+              height: "100vh",
+              width: "100vw",
+              minHeight: "-webkit-fill-available",
+              position: "fixed"
             }}
-            onClick={() => {
-              props.setSelectedTag({ lat: tag.lat, lng: tag.lng, name: tag.name, desc: tag.desc, img: tag.img })
-              props.setOpenInfo(true)
-            }
-            }
+            opts={{
+              center: props.cent,
+              zoom: 6,
+              disableDefaultUI: false,
+              fullscreenControl: false,
+              streetViewControl: false
+            }}
           />
-        ))}
-      </GoogleMapProvider>
-    </LoadingOverlay>
+          <CustomControl bindingPosition="BOTTOM_CENTER">
+            <Button variant="contained" color="primary" onClick={() => props.setOpenPlace(true)} style={{ marginBottom: "20px" }}>
+              Place tag!
+                </Button>
+          </CustomControl>
+
+          {props.tags.map(tag => (
+            <Marker
+              key={tag._id}
+              id={tag.name}
+              opts={{
+                position: {
+                  lat: tag.lat,
+                  lng: tag.lng
+                }
+              }}
+              onClick={() => {
+                props.setSelectedTag({ lat: tag.lat, lng: tag.lng, name: tag.name, desc: tag.desc, img: tag.img })
+                props.setOpenInfo(true)
+              }
+              }
+            />
+          ))}
+        </GoogleMapProvider>
+      </LoadingOverlay>
+    </div>
   )
 };
